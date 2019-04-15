b677ae31ea657a48477ca3f3f6c03d98e488660e
Jouni Sunnari <Jouny@tuta.io>
2019-04-15 21:09:21 +0300

    added id to news

--- news.php
+++ news.php
@@ -1,12 +1,14 @@
 <?php
 
 class News {
+    public $id = 0;
     public $title = "";
     public $content = "";
 
-    public function __construct ($title, $content) {
+    public function __construct ($title, $content, $id) {
         $this->title = $title;
         $this->content = $content;
+        $this->id = $id;
     }
 }
 
@@ -16,15 +18,15 @@ class NewsDatabase {
     }
 
     public function get($from, $to) {
-        $result = $this->connection->query("select title, content from news limit " . $from . ", " . $to . ";");
+        $result = $this->connection->query("select title, content, id from news limit " . $from . ", " . $to . ";");
         while ($row = $result->fetch_assoc())
-            yield new News($row['title'], $row['content']);
+            yield new News($row['title'], $row['content'], $row['id']);
     }
 
     public function get_one($id) {
-        $result = $this->connection->query("select title, content from news where id = " . $id . ";");
+        $result = $this->connection->query("select title, content, id from news where id = " . $id . ";");
         $row = $result->fetch_assoc();
-        return new News($row['title'], $row['content']);
+        return new News($row['title'], $row['content'], $row['id']);
     }
 }
 
